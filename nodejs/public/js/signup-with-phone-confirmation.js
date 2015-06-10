b66c755d36e78cd9c4ec80db0deda188711b7484
Arie Benichou <arie.benichou@gmail.com>
2015-06-10 12:50:00 +0200

    updated sms for activation code

--- nodejs/public/js/signup-with-phone-confirmation.js
+++ nodejs/public/js/signup-with-phone-confirmation.js
@@ -11,5 +11,5 @@ var phone = GET.phone.replace(/\+/g, " ");
 document.getElementById("phone").innerHTML = phone;
 
 document.getElementById("oui").addEventListener("click", function() {
-  promise.get("http://78.193.235.5/sms/" + phone + "/test from ui");
+  promise.get("http://78.193.235.5:3000/sms/" + phone + "/" + "Bienvenue chez omerxi!" + "%0A" + "Votre code d'activation est 123-456.");
 }, false);
